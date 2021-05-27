609dc9f120a89874845998235b82196e944bca7b
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-05-27 13:11:00 +0000

    Deploying to gh-pages from @ IiIymik/CRUD@7d2123957e8427ca41959ae4326c6fe046bd3af9 🚀

--- src.a12e48be.js
+++ src.a12e48be.js
@@ -1,10 +1,10 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
 },{}],"MuPq":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.BASE_URL=void 0;var e="http://localhost:3000";exports.BASE_URL=e;
-},{}],"Jhpy":[function(require,module,exports) {
-"use strict";var n=require("./constants");function t(t){var r="".concat(n.BASE_URL,"/person/").concat(t);return fetch(r,{method:"DELETE"}).then(function(n){return n.json()})}
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.BASE_URL=void 0;const e="http://localhost:3000";exports.BASE_URL=e;
+},{}],"wuxJ":[function(require,module,exports) {
+"use strict";var t=require("./constants");async function n(){try{const c=await fetch(`${t.BASE_URL}/person`);return await c.json()}catch(n){console.log(n)}}async function c(n){try{const c=await fetch(`${t.BASE_URL}/person/${n}`);return await c.json()}catch{console.log(Error)}}n(),c(2),c(4);
 },{"./constants":"MuPq"}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss"),require("./js/delete.js");
-},{"./sass/main.scss":"clu1","./js/delete.js":"Jhpy"}]},{},["Focm"], null)
-//# sourceMappingURL=/CRUD/src.94da403e.js.map
+"use strict";require("./sass/main.scss"),require("./js/get.js");
+},{"./sass/main.scss":"clu1","./js/get.js":"wuxJ"}]},{},["Focm"], null)
+//# sourceMappingURL=/CRUD/src.a12e48be.js.map
